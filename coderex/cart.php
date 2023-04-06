cb505344a2831758c25b307650fdde1af5ee528c
anik4997 <44748778+anik4997@users.noreply.github.com>
2023-04-06 14:43:58 +0600

    Update cart.php

--- coderex/cart.php
+++ coderex/cart.php
@@ -1,10 +1,5 @@
 <?php
-// Database connection start
-$connect = mysqli_connect('localhost','coderex','Coderex@12345','products');
-if(!$connect){
-  die("Not connected".mysqli_error());
-}
-//  Database connection end
+include 'database_connection.php';
 if(isset($_POST['select_items'])){
   $quantity = $_POST['quantity']; 
   
